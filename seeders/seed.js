cca1467812cc24cb0bd2744d4f1ef9f3569c0917
Santosh Malaki <santoshmalaki@Santoshs-MacBook-Pro.local>
2021-05-08 14:21:47 -0400

    1qtJ4P

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/Workout", {
+mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
